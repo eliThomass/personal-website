40e85f408d3f7c22e171b85fc8a733bc5d3f82bd
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2026-08-25 18:12:55 -0700

    ran npm audit and updated resume again

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,8 +2,8 @@ import RESUME from '../assets/Elias_Thomas_Resume.jpg';
 import '../styles/resume.css';
 function Resume() {
   return (
-    <div class='resume'>
-      <img src={RESUME} class='resumeimg'></img>
+    <div className='resume'>
+      <img src={RESUME} className='resumeimg' alt='Resume' />
     </div>
   )
 }
